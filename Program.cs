108ea1d0a126cd701f69f1ac42496464cdab3b80
Bartolomeo707 <162818251+Bartolomeo707@users.noreply.github.com>
2024-03-09 17:47:30 +0100

    HUUUGE INTS

--- Program.cs
+++ Program.cs
@@ -2,7 +2,37 @@
 using System.Numerics;
 
 namespace ZajzeSlot {
+
     class Program {
+
+        static BigInteger Sqrt(BigInteger n)
+        {
+            if (n == 0) return 0;
+            if (n > 0)
+            {
+                int bitLength = Convert.ToInt32(Math.Ceiling(BigInteger.Log(n, 2)));
+                BigInteger root = BigInteger.One << (bitLength / 2);
+
+                while (!isSqrt(n, root))
+                {
+                    root += n / root;
+                    root /= 2;
+                }
+
+                return root;
+            }
+
+            throw new ArithmeticException("NaN");
+        }
+
+        private static Boolean isSqrt(BigInteger n, BigInteger root)
+        {
+            BigInteger lowerBound = root * root;
+            BigInteger upperBound = (root + 1) * (root + 1);
+
+            return (n >= lowerBound && n < upperBound);
+        }
+
         static BigInteger Silnia(int n) {
             BigInteger wynik = 1;
             int indeks = n;
@@ -94,7 +124,7 @@ namespace ZajzeSlot {
             uint n = 3;
             Console.WriteLine(IntPow(2, n) - 1);
             while (true) {
-                BigInteger i = IntPow(2, n) - 2;
+                BigInteger i = Sqrt((IntPow(2, n) - 1));
                 bool CzyPierwsza = true;
                 while (i >= 2)
                 {
